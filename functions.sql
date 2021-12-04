60664174b0a45241fcf1b74effd2f536b46910fc
Bobur_Zokirov <zokirovbobur93@gmail.com>
2021-12-04 20:45:21 +0500

    task 6

--- functions.sql
+++ functions.sql
@@ -10,3 +10,18 @@ as $$
 create trigger update_time_trigger
     before update on students for each row
     execute procedure update_time();
+
+--6
+create function user_name_validation() returns trigger language plpgsql
+as $$
+    begin
+        if NEW.name like '%@%' or NEW.name like '%#%' or NEW.name like '%$%' then
+            return null;
+        end if;
+        return NEW;
+    end;
+    $$;
+
+create trigger user_name_validation_trigger
+    before insert on students for each row
+    execute procedure user_name_validation();
